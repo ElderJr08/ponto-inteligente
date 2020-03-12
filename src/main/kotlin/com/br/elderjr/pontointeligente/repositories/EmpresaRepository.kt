8ab98ceb6d6645b2476ad290dfedf0cd985fd72f
Elder Junior <elderjunior08@gmail.com>
2020-03-11 22:53:39 -0300

    Implementa criação de repositories (empresa, funcionario e lançamento)

---
+++ src/main/kotlin/com/br/elderjr/pontointeligente/repositories/EmpresaRepository.kt
@@ -0,0 +1,8 @@
+package com.br.elderjr.pontointeligente.repositories
+
+import com.br.elderjr.pontointeligente.documents.Empresa
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface EmpresaRepository : MongoRepository<Empresa, String> {
+    fun findByCnpj(cnpj: String): Empresa
+}
---
+++ src/main/kotlin/com/br/elderjr/pontointeligente/repositories/FuncionarioRepository.kt
@@ -0,0 +1,9 @@
+package com.br.elderjr.pontointeligente.repositories
+
+import com.br.elderjr.pontointeligente.documents.Funcionario
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface FuncionarioRepository : MongoRepository<Funcionario, String> {
+    fun findByEmail(email: String): Funcionario
+    fun findByCpf(cpf: String): Funcionario
+}
